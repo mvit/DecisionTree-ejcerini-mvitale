6c9565d8ef3a9b89e727e7b36d174c9f754ff359
Maurizio Vitale <mauriziovit@gmail.com>
2016-09-18 17:55:10 -0400

    Added feature reading
    It's untested because

--- DecisionTree.py
+++ DecisionTree.py
@@ -9,7 +9,7 @@ def infoGain(outcomes, features):
 
     for outcome in outcomes:
         idx = outcomes.index(outcome)
-        feature = features{idx}
+        feature = features[idx]
         if outcome.equals('1'):
             countlist[feature][0] += 1
         elif outcome.equals('2'):
@@ -26,12 +26,26 @@ def main(argv):
         #Do feature recognition
         labels = file.readline().strip().split(',')
         feat_idx = labels.index('winner')
-        features = labels[feat_idx + 1:]
+        featurenames = labels[feat_idx + 1:]
         print(features)
         
+        outcomes = []
+        features = {}
+        
+        #Add arrays per feature
+        for name in featurenames:
+            features[name] = []
+
         #Do feature reading
         for line in file:
-            
+            board = line.strip().split(',')
+            print(board)
+            outcomes.append(board[feat_idx])
+
+            for name in featurenames:
+                name_idx = featurenames.index(name)
+                features[name].append(board[feat_idx + name_idx])
+
         #Build the tree by information gain
         for feature in features:
             
