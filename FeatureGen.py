3cbf56d2d8d79d4b8dc8d8a565e11bfd022c44e6
Maurizio Vitale <mauriziovit@gmail.com>
2016-09-18 16:07:19 -0400

    Fixed Comma Separation in FeatureGen

--- FeatureGen.py
+++ FeatureGen.py
@@ -3,19 +3,26 @@ import sys
 def main(argv):
     #Every 6 digits is a column
     if (len(argv) < 2):
-        print("python featuregen.py infile.csv outfile.csv")
+        print("python FeatureGen.py infile.csv outfile.csv")
     
     with open(argv[0], 'r') as file:
         outfile = open(argv[1], 'w')
         #Parse labels
         labels = file.readline().strip().split(',')
         print(labels)
-        labels.append('lowerleft')
+        labels.append('bottomleft')
         labels.append('p1center')
         labels.append('p2center')
+        labels.append('p13inrow')
+        labels.append('p23inrow')
+        labels.append('p1btmcount')
+        labels.append('p2btmcount')
+        
         
         for label in labels:
-            outfile.write(label + ',')
+            outfile.write(label)
+            if (labels.index(label) < (len(labels)-1)):
+                outfile.write(', ')
         
         outfile.write('\n')
         
